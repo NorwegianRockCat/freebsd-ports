89f41c03399dd82e8ca64cdbcbbe32bcb09bfae1
Trenton Schulz <trenton@norwegianrockcat.com>
2021-07-28 21:02:25 +0200

    Update Gazebo port.
    Given how Filter is implemented, it works with to pass en "empty"
    EventFilter here.

--- cad/gazebo/files/patch-gazebo_gui_MouseEventHandler.cc
+++ cad/gazebo/files/patch-gazebo_gui_MouseEventHandler.cc
@@ -1,11 +1,11 @@
---- gazebo/gui/MouseEventHandler.cc.orig	2021-04-02 01:08:12 UTC
+--- gazebo/gui/MouseEventHandler.cc.orig	2021-07-28 18:53:01 UTC
 +++ gazebo/gui/MouseEventHandler.cc
 @@ -121,7 +121,7 @@ void MouseEventHandler::Add(const std::string &_name,
  void MouseEventHandler::Remove(const std::string &_name,
      std::list<Filter> &_list)
  {
 -  _list.remove(Filter(_name, NULL));
-+  _list.remove(Filter(_name, 0));
++  _list.remove(Filter(_name, MouseEventFilter()));
  }
  
  /////////////////////////////////////////////////
