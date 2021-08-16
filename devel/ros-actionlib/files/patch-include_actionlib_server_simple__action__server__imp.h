ecc04301301e591262b63fe6b502862bff497c6e
Trenton Schulz <trenton@norwegianrockcat.com>
2021-08-16 17:58:22 +0200

    Update the actionlib patches
    It seems that we cannot assign a nullptr straight ot this function. So, just let it get be empty.

--- devel/ros-actionlib/files/patch-include_actionlib_server_simple__action__server__imp.h
+++ devel/ros-actionlib/files/patch-include_actionlib_server_simple__action__server__imp.h
@@ -1,20 +1,21 @@
 --- include/actionlib/server/simple_action_server_imp.h.orig	2020-08-21 15:47:46 UTC
 +++ include/actionlib/server/simple_action_server_imp.h
-@@ -64,7 +64,7 @@ SimpleActionServer<ActionSpec>::SimpleActionServer(std
+@@ -63,8 +63,7 @@ SimpleActionServer<ActionSpec>::SimpleActionServer(std
+ 
  template<class ActionSpec>
  SimpleActionServer<ActionSpec>::SimpleActionServer(std::string name, bool auto_start)
- : new_goal_(false), preempt_request_(false), new_goal_preempt_request_(false), execute_callback_(
+-: new_goal_(false), preempt_request_(false), new_goal_preempt_request_(false), execute_callback_(
 -    NULL), execute_thread_(NULL), need_to_terminate_(false)
-+    nullptr), execute_thread_(nullptr), need_to_terminate_(false)
++: new_goal_(false), preempt_request_(false), new_goal_preempt_request_(false), execute_thread_(nullptr), need_to_terminate_(false)
  {
    // create the action server
    as_ = boost::shared_ptr<ActionServer<ActionSpec> >(new ActionServer<ActionSpec>(n_, name,
-@@ -117,7 +117,7 @@ template<class ActionSpec>
+@@ -117,7 +116,7 @@ template<class ActionSpec>
  SimpleActionServer<ActionSpec>::SimpleActionServer(ros::NodeHandle n, std::string name,
    bool auto_start)
  : n_(n), new_goal_(false), preempt_request_(false), new_goal_preempt_request_(false),
 -  execute_callback_(NULL), execute_thread_(NULL), need_to_terminate_(false)
-+  execute_callback_(nullptr), execute_thread_(nullptr), need_to_terminate_(false)
++  execute_thread_(nullptr), need_to_terminate_(false)
  {
    // create the action server
    as_ = boost::shared_ptr<ActionServer<ActionSpec> >(new ActionServer<ActionSpec>(n, name,
